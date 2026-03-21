80a675e74274c5ae5e589c5d28fb7a8961c52e63
JKrishnaD <jayak5063@gmail.com>
2026-03-21 17:45:10 +0530

    feat: impl add liquidity ix

--- programs/dlmm/src/error.rs
+++ programs/dlmm/src/error.rs
@@ -1,7 +1,27 @@
 use anchor_lang::prelude::*;
 
 #[error_code]
-pub enum ErrorCode {
+pub enum DlmmErrors {
     #[msg("Custom error message")]
     CustomError,
+    #[msg("Invalid bin range")]
+    InvalidBinRange,
+    #[msg("Bin range exceeds maximum of 70 bins per position")]
+    RangeExceedMaxBins,
+    #[msg("Zero amount")]
+    ZeroAmount,
+    #[msg("Invalid token amount for bin range")]
+    InvalidTokenAmount,
+    #[msg("Math overflow")]
+    MathOverflow,
+    #[msg("Bin array not found")]
+    BinArrayNotFound,
+    #[msg("Zero shares")]
+    ZeroShares,
+    #[msg("Invalid bin array")]
+    InvalidBinArray,
+    #[msg("Out of bounds")]
+    OutOfBounds,
+    #[msg("Invalid bin index")]
+    InvalidBinIndex,
 }
